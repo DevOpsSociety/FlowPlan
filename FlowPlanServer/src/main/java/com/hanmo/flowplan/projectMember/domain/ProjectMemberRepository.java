e849323c65098e255604848901ac982b6d99d7eb
taenam kim <ktn2674@naver.com>
2025-12-08 16:25:21 +0900

    fix(projectMember): ProjectMemberResponse.from(member) 메서드 안에서 member.getUser().getName()을 호출할 때, User 정보를 DB에서 가져오려는데 이미 트랜잭션(Session)이 닫혀 있어서 실패하는 문제 해결

--- FlowPlanServer/src/main/java/com/hanmo/flowplan/projectMember/domain/ProjectMemberRepository.java
+++ FlowPlanServer/src/main/java/com/hanmo/flowplan/projectMember/domain/ProjectMemberRepository.java
@@ -3,12 +3,16 @@ package com.hanmo.flowplan.projectMember.domain;
 import com.hanmo.flowplan.project.domain.Project;
 import com.hanmo.flowplan.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
-  List<ProjectMember> findAllByProjectId(Long projectId);
+
+  @Query("SELECT pm FROM ProjectMember pm JOIN FETCH pm.user WHERE pm.project.id = :projectId")
+  List<ProjectMember> findAllByProjectId(@Param("projectId") Long projectId);
   List<ProjectMember> findAllByUser(User user);
   Optional<ProjectMember> findByUserAndProject(User user, Project project);
   boolean existsByUserAndProject(User user, Project project);
